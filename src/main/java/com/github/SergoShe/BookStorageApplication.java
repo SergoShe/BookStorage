549f61ad8f60fccd92f8d9473aeadddeb59dd052
SergoShe <snow5373@gmail.com>
2023-07-11 13:11:03 +0700

    init repository

---
+++ src/main/java/com/github/SergoShe/BookStorageApplication.java
@@ -0,0 +1,7 @@
+package com.github.SergoShe;
+
+public class BookStorageApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/github/SergoShe/model/Book.java
@@ -0,0 +1,40 @@
+package com.github.SergoShe.model;
+
+public class Book {
+    private long id;
+    private String title;
+    private String author;
+    private int year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
