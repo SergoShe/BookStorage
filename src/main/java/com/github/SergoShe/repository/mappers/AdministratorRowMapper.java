587ca4ee86bad2cbf9edd93b7a36e4543434ff8e
SergoShe <snow5373@gmail.com>
2023-10-18 22:40:30 +0700

    Небольшое исправление

--- src/main/java/com/github/SergoShe/repository/mappers/AdministratorRowMapper.java
+++ src/main/java/com/github/SergoShe/repository/mappers/AdministratorRowMapper.java
@@ -1,6 +1,7 @@
 package com.github.SergoShe.repository.mappers;
 
 import com.github.SergoShe.model.Administrator;
+import com.github.SergoShe.repository.constant.AdministratorConstant;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -11,12 +12,11 @@ public class AdministratorRowMapper implements RowMapper<Administrator> {
     @Override
     public Administrator mapRow(ResultSet resultSet, int rowNum) throws SQLException {
         Administrator admin = new Administrator();
-        admin.setAdministratorId(resultSet.getLong("administrator_id"));
-        admin.setName(resultSet.getString("name"));
-        admin.setSurname(resultSet.getString("surname"));
-        admin.setCreateDate(LocalDate.parse(resultSet.getString("create_date")));
-        admin.setUpdateDate(LocalDate.parse(resultSet.getString("update_date")));
-
+        admin.setAdministratorId(resultSet.getLong(AdministratorConstant.ADMINISTRATOR_ID_COLUMN));
+        admin.setName(resultSet.getString(AdministratorConstant.NAME_COLUMN));
+        admin.setSurname(resultSet.getString(AdministratorConstant.SURNAME_COLUMN));
+        admin.setCreateDate(LocalDate.parse(resultSet.getString(AdministratorConstant.CREATE_DATE_COLUMN)));
+        admin.setUpdateDate(LocalDate.parse(resultSet.getString(AdministratorConstant.UPDATE_DATE_COLUMN)));
         return admin;
     }
 }
